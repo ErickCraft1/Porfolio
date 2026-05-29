09fcd56426ad5cabee11911201ec1ef3283d90f2
erick <yusske06@gmail.com>
2026-05-29 00:00:15 -0500

    fix: Solution updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,9 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   output: "server",
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
 });
